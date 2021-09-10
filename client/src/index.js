d69c80fb12915eea999cab9b319b87a215099acc
moonshadow <last_enemy@naver.com>
2021-09-10 15:06:20 +0900

    init

---
+++ client/src/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import AuthService from "./service/auth";
+import TweetService from "./service/tweet";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import { AuthErrorEventBus } from "./context/AuthContext";
+import HttpClient from "./network/http";
+import TokenStorage from "./db/token";
+import socket, { io } from "socket.io-client";
+
+const baseURL = process.env.REACT_APP_BASE_URL;
+const tokenStorage = new TokenStorage();
+const httpClient = new HttpClient(baseURL);
+const authErrorEventBus = new AuthErrorEventBus();
+const authService = new AuthService(httpClient, tokenStorage);
+const tweetService = new TweetService(httpClient, tokenStorage);
+
+const socketIO = socket(baseURL);
+socketIO.on("connection_error", (error) => {
+  console.log("socket error", error);
+});
+
+io.on("dwitter", (message) => {
+  console.log(message);
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider
+        authService={authService}
+        authErrorEventBus={authErrorEventBus}
+      >
+        <App tweetService={tweetService} />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
