504c7ae7b5131f4bb8aa2aaecb87e581139abf9e
moonshadow <last_enemy@naver.com>
2021-10-06 14:45:10 +0900

    Sequlize

--- server/data/auth.js
+++ server/data/auth.js
@@ -1,18 +1,46 @@
-import { db } from '../db/database.js';
+import SQ from 'sequelize';
+import { db, sequelize } from '../db/database.js';
+const DataTypes = SQ.DataTypes;
+
+const User = sequelize.define(
+  'user',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING(45),
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+    },
+  },
+  { timestamps: false }
+);
 
 export async function findByUsername(username) {
-  return db
-    .execute('SELECT * FROM users WHERE username=?', [username]) //
-    .then((result) => result[0][0]);
+  return User.findOne({ where: { username } });
 }
 
 export async function findById(id) {
-  return db
-    .execute('SELECT * FROM users WHERE id=?', [id]) //
-    .then((result) => result[0][0]);
+  return User.findByPk(id);
 }
 
 export async function createUser(user) {
+  return User.create(user).then((data) => data.dataValues.id);
   const { username, password, name, email, url } = user;
   return db
     .execute(
